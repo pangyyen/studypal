19c0f61ac3b2018e0cf85e1b998a54bfd7079511
Shi Hong <68313416+chuashihong@users.noreply.github.com>
2023-07-07 00:53:14 +0800

    module page setup

--- src/scenes/sidebar-scenes/Module.jsx
+++ src/scenes/sidebar-scenes/Module.jsx
@@ -1,20 +1,23 @@
-import {
-    Box,
-    Button,
-    ButtonBase,
-    TextField,
-    Typography,
-    Select,
-    MenuItem,
-    ButtonGroup,
-} from "@mui/material";
+import { Box, Button, Typography, ButtonGroup } from "@mui/material";
 
-import React from "react";
+import {React, useState} from "react";
 import Sidebar from "../../components/SideBar";
-
 const Module = () => {
     const moduleCode = "HSI 1001";
     const moduleFullName = "How Science Work, Why Science Work";
+    const [page, setPage] = useState("study-jios");
+    const handleStudyJios = () => {
+        setPage("study-jios");
+    };
+    const handleSharedFiles = () => {
+        setPage("shared-files");
+    };
+    const handleTelegramChats = () => {
+        setPage("telegram-chats");
+    };
+    const handleGeneralDiscussion = () => {
+        setPage("general-discussion");
+    };
     return (
         <Box display="flex">
             <Sidebar />
@@ -37,7 +40,10 @@ const Module = () => {
                 </Box>
                 <div className="flex">
                     <div class="mx-1 flex">
-                        <button className="bg-gray-700 rounded-xl text-white p-4 flex m-3">
+                        <button
+                            className="bg-gray-700 rounded-xl text-white p-4 flex m-3"
+                            onClick={handleStudyJios}
+                        >
                             Study Jios
                             <div className="rounded-full bg-blue-600 w-5 h-5 ml-2">
                                 2
@@ -46,7 +52,10 @@ const Module = () => {
                         <div class="h-10 border-l-4 border-solid border-blue-500 rounded my-5 ml-[-1rem]"></div>
                     </div>
                     <div class="mx-1 flex">
-                        <button className="bg-gray-700 rounded-xl text-white p-4 flex m-3">
+                        <button
+                            className="bg-gray-700 rounded-xl text-white p-4 flex m-3"
+                            onClick={handleSharedFiles}
+                        >
                             Shared Files
                             <div className="rounded-full bg-purple-600 w-5 h-5 ml-2">
                                 3
@@ -55,7 +64,10 @@ const Module = () => {
                         <div class="h-10 border-l-4 border-solid border-purple-500 rounded my-5 ml-[-1rem]"></div>
                     </div>
                     <div class="mx-1 flex">
-                        <button className="bg-gray-700 rounded-xl text-white p-4 flex m-3">
+                        <button
+                            className="bg-gray-700 rounded-xl text-white p-4 flex m-3"
+                            onClick={handleTelegramChats}
+                        >
                             Telegram Chats
                             <div className="rounded-full bg-red-600 w-5 h-5 ml-2">
                                 2
@@ -64,7 +76,10 @@ const Module = () => {
                         <div class="h-10 border-l-4 border-solid border-red-500 rounded my-5 ml-[-1rem]"></div>
                     </div>
                     <div class="mx-1 flex">
-                        <button className="bg-gray-700 rounded-xl text-white p-4 flex m-3">
+                        <button
+                            className="bg-gray-700 rounded-xl text-white p-4 flex m-3"
+                            onClick={handleGeneralDiscussion}
+                        >
                             General Discussion
                             <div className="rounded-full bg-yellow-600 w-5 h-5 ml-2">
                                 2
@@ -73,14 +88,44 @@ const Module = () => {
                         <div class="h-10 border-l-4 border-solid border-yellow-500 rounded my-5 ml-[-1rem]"></div>
                     </div>
                 </div>
-                <ButtonGroup>
-                    <Button className="border border-white text-white">
-                        <Typography color="primary">Study Jios</Typography>
-                    </Button>
-                    <Button variant="outlined">Study Jios</Button>
-                    <Button variant="outlined">Study Jios</Button>
-                    <Button variant="outlined">General Discussion</Button>
-                </ButtonGroup>
+                <div className="flex">
+                    {page === "study-jios" && (
+                        <div>
+                            <div className="flex justify-between">
+                                <Typography variant="h2">
+                                    Study Jios
+                                </Typography>
+                            </div>
+                        </div>
+                    )}
+                    {page === "shared-files" && (
+                        <div>
+                            <div className="flex justify-between">
+                                <Typography variant="h2">
+                                    Shared Files
+                                </Typography>
+                            </div>
+                        </div>
+                    )}
+                    {page === "telegram-chats" && (
+                        <div>
+                            <div className="flex justify-between">
+                                <Typography variant="h2">
+                                    Telegram Chats
+                                </Typography>
+                            </div>
+                        </div>
+                    )}
+                    {page === "general-discussion" && (
+                        <div>
+                            <div className="flex justify-between">
+                                <Typography variant="h2">
+                                    General Discussion
+                                </Typography>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </Box>
         </Box>
     );
